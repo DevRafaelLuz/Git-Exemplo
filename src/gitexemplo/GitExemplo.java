552eae920ab1aecbca3a58f129fd11432163d858
RafaelLuz <rafael.luz2005@gmail.com>
2023-07-05 19:19:23 -0300

    Mudando saída da mensagem

--- src/gitexemplo/GitExemplo.java
+++ src/gitexemplo/GitExemplo.java
@@ -1,9 +1,11 @@
 package gitexemplo;
 
+import javax.swing.JOptionPane;
+
 public class GitExemplo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null, "Hello World");
     }
     
 }
